e6917f400995e54c9d1ed33dd65b6bcdd92446ce
Baibhav Singh <dev.baibhav.singh@gmail.com>
2022-09-05 10:19:53 +0530

    Initial commit

---
+++ SmartNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartNews
+//
+//  Created by Baibhav singh on 05/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
